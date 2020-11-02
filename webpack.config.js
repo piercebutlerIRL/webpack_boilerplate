7ce89ce111261f3fff485b8ed12f7dbb1dd16cbd
Pierce <pierce@Pierces-MacBook-Pro-3.local>
2020-11-02 20:32:22 +0000

    added sass to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
   output: {
-    path: path.resolve(__dirname, 'public/scripts'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    filename: 'scripts/bundle.js',
   },
   module: {
     rules: [
@@ -17,8 +18,22 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+  ],
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     publicPath: '/scripts/',
